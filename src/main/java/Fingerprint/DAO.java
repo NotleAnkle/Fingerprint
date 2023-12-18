e28db2969a3d6be7940a16c1dc3fcdcc28e2bdfe
Nguyễn Minh Tuân <tuannm@zent.vn>
2023-12-18 11:46:04 +0700

    create attendance feature

--- src/main/java/Fingerprint/DAO.java
+++ src/main/java/Fingerprint/DAO.java
@@ -4,19 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DAO {
-	public static Connection con;
-	
-	public DAO(){
-		if(con == null){
-			String dbUrl = "jdbc:mysql://localhost:3306/finger?autoReconnect=true&useSSL=false";
-			String dbClass = "com.mysql.cj.jdbc.Driver";
+    public static Connection con;
 
-			try {
-				Class.forName(dbClass);
-				con = DriverManager.getConnection (dbUrl, "root", "12345678");
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public DAO() {
+        if (con == null) {
+//            String dbUrl = "jdbc:mysql://localhost:3306/finger?autoReconnect=true&useSSL=false";
+            String dbUrl = "jdbc:mysql://localhost:3306/finger";
+            String dbClass = "com.mysql.cj.jdbc.Driver";
+
+            try {
+                Class.forName(dbClass);
+                con = DriverManager.getConnection(dbUrl, "root", "Tunham123456789");
+                System.out.println("database connected");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
